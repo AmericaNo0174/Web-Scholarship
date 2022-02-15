6ea184fd15055ce00aa6d64ddf0f039cf8e98f35
AmericaNo0174 <boyjame201021@gmail.com>
2022-02-15 15:16:05 +0700

    Revert "up date login"
    This reverts commit 13381da3c18f5c8144a719ec939f9b4ec7d56a2d.

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,55 +2,34 @@
   <div class="loginapp">
       <div id="container">
         <!-- <br><br><br> -->
-        <div class="boxlogin2">
+        <div>
             <center>
               <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/ENG_th-flat_transparent_%281%29.gif" width="150" height="150" alt="">
               <h1>SIGN IN</h1>
               <br>
               <form @submit.prevent="login">
-                  <div class="txt_field">
-                      <p><i class="fas fa-user"></i><input v-model="username" required placeholder="Username/Email" type="text" name="" id=""></p>
-                  </div>
-                   <div class="txt_field">
-                      <p><i class="fas fa-key"></i><input v-model="username" required placeholder="Password" type="password" name="" id=""></p>
-                  </div>
-                
+                <p><input v-model="username" required placeholder="Username/Email" type="text" name="" id=""></p>
+                <p><input v-model="username" required placeholder="Password" type="password" name="" id=""></p>
                 <br>
                 <p><button type="submit">Login</button></p>
               </form>
             </center>
           </div>
- 
-          <div class="boxlogin">
-            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img src="https://reg.src.ku.ac.th/res/image/reg64_2.jpg" class="d-block w-100" width="300" height="300" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://reg.src.ku.ac.th/res/image/reg64_2.jpg" class="d-block w-100" width="300" height="300" alt="...">
-              </div>
-              <div class="carousel-item">
-                <img src="https://reg.src.ku.ac.th/res/image/reg64_2.jpg" class="d-block w-100" width="300" height="300" alt="...">
-              </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </button>
-          </div>
+          <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/ENG_th-flat_transparent_%281%29.gif" width="150" height="150" alt="">
+              <h1>SIGN IN</h1>
+              <br>
+              <form @submit.prevent="login">
+                <p><input v-model="username" required placeholder="Username/Email" type="text" name="" id=""></p>
+                <p><input v-model="username" required placeholder="Password" type="password" name="" id=""></p>
+                <br>
+                <p><button type="submit">Login</button></p>
+              </form>
           </div>
       </div>
-
+      <!--nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav-->
      
 
     </div>
@@ -68,73 +47,45 @@ export default {
   max-height: 100%;
   /* border: 1px solid rgb(255, 0, 0); */
   font-weight: normal;
-  
   /* justify-content:space-between; */
 }
 #container{
   width: 100%;
-  height: 100vh;
-  /* border: 1px solid red; */
+  height: 100%;
+  border: 1px solid red;
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 10px;
+  gap: 100px;
   /* border: 1px solid red; */
   /* background-image: url('assets/123.jpg') ; */
 }
-.boxlogin{
-  width:50%;
-  height:100%;
-  background-image:url('../../assets/123.jpg');
-  display:flex;
-  align-items:center;
-  justify-content:center; 
-}
-.boxlogin2{
-  width:50%;
-  height:100%;
+/* body{
   background-color:rgb(255, 255, 255);
-  display:flex;
-  align-items:center;
-  justify-content:center; 
+}
+center {
+  bottom:550px;
+  
 }
 h1{
-  margin-top:30px;
-  color:#680c07;
-  font-size:17.37px;
-  font-style: Roboto;
+  text-align:center;
+  color:#680C07;
 }
-center{
-  margin-bottom:10px;
+h2{
+  text-align:center;
+  color:#ffffff;
 }
+
 button{
-  font-size:15px;
-  font-style:Roboto;
-  padding:13px;
-  width:130px;
-  border-radius: 25px 25px 25px 25px;
-  color:#FFFFFF;
-  background-color:#680c07;
+  text-align:center;
+  color:#FFFF;
+  background-color:#680C07;
+  border-radius:15px 15px 15px 15px;
+  padding:1px;
+  width:100px;
+  height: 50px;
+  font-style: bold;
 
-}
-.center form{
-  padding:0 40px;
-  box-sizing: border-box;
-}
-form .txt_field{
-  position: relative;
-  border-bottom:2px solid #adadad;
-  margin: 30px 0;
-
-}
-.txt_field input{
-  width:60vh;
-  padding: 0 5px;
-  height:40px;
-  font-size: 16px;
-  border: none;
-  background:none;
-  outline:none;
-}
+} */
 
 </style>
